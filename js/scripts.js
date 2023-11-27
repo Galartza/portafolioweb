15aa6ff283d2a215057806791f8a1637a1a73211
joa <jotaquin99g@gmail.com>
2023-11-27 15:04:46 -0300

    inicio+nabvar1.1

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
     const overlay = document.getElementById('overlay');
+    const btnCv = document.querySelector(".btn-cv");
 
     abrir.addEventListener('click', () => {
         nav.classList.add("visible");
-        overlay.style.display = 'block';
+        overlay.classList.add("visible");
+        btnCv.classList.add("no-hover");
     });
 
     cerrar.addEventListener('click', () => {
         nav.classList.remove("visible");
-        overlay.style.display = 'none';
+        overlay.classList.remove("visible");
+        btnCv.classList.remove("no-hover");
     });
 
     overlay.addEventListener('click', (event) => {
         // Solo cierra el menú si se hace clic directamente en el overlay (evita que se cierre al hacer clic en elementos internos)
         if (event.target === overlay) {
             nav.classList.remove("visible");
-            overlay.style.display = 'none';
+            overlay.classList.remove("visible");
+            btnCv.classList.remove("no-hover");
         }
     });
 });
-
