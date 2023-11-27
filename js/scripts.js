64acdf0195fce0a3c68d47d323644cec2597340b
joa <jotaquin99g@gmail.com>
2023-11-27 14:25:15 -0300

    inicio1.2

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const nav = document.querySelector("#nav");
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
+    const overlay = document.getElementById('overlay');
 
     abrir.addEventListener('click', () => {
         nav.classList.add("visible");
-    })
+        overlay.style.display = 'block';
+    });
 
     cerrar.addEventListener('click', () => {
         nav.classList.remove("visible");
-    })
+        overlay.style.display = 'none';
+    });
+
+    overlay.addEventListener('click', (event) => {
+        // Solo cierra el menú si se hace clic directamente en el overlay (evita que se cierre al hacer clic en elementos internos)
+        if (event.target === overlay) {
+            nav.classList.remove("visible");
+            overlay.style.display = 'none';
+        }
+    });
 });
+
