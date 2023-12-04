5db435d90e2f03730a91f1067ba65f70cb771961
joa <jotaquin99g@gmail.com>
2023-12-04 16:27:45 -0300

    vFinal1.1

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const nav = document.querySelector("#nav");
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
-    const overlay = document.getElementById('overlay');
     const btnCv = document.querySelector(".btn-cv");
     const header = document.getElementById('header');
     
-        let prevScrollPos = window.scrollY || window.pageYOffset;
+        let prevScrollPos = window.scrollY;
     
         function handleScroll() {
-            let currentScrollPos = window.scrollY || window.pageYOffset;
+            let currentScrollPos = window.scrollY;
     
             if (prevScrollPos > currentScrollPos) {
                 // Hacer algo al hacer scroll hacia arriba
@@ -26,25 +25,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     abrir.addEventListener('click', () => {
         nav.classList.add("visible");
-        overlay.classList.add("visible");
         btnCv.classList.add("no-hover");
     });
 
     cerrar.addEventListener('click', () => {
-        nav.classList.remove("visible");
-        overlay.classList.remove("visible");
+        nav.classList.remove("visible");     
         btnCv.classList.remove("no-hover");
     });
 
-    overlay.addEventListener('click', (event) => {
-        // Solo cierra el menú si se hace clic directamente en el overlay (evita que se cierre al hacer clic en elementos internos)
-        if (event.target === overlay) {
-            nav.classList.remove("visible");
-            overlay.classList.remove("visible");
-            btnCv.classList.remove("no-hover");
-        }
-    });
-
     window.addEventListener('scroll', handleScroll);
-
 });
+
+
+
+
+
