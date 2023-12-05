7196d76f843599e654a022877baba4d2e3b93a77
joa <jotaquin99g@gmail.com>
2023-12-05 14:19:59 -0300

    dando retoques finales

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,83 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar elementos del DOM
     const nav = document.querySelector("#nav");
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
     const btnCv = document.querySelector(".btn-cv");
     const header = document.getElementById('header');
-    
-        let prevScrollPos = window.scrollY;
-    
-        function handleScroll() {
-            let currentScrollPos = window.scrollY;
-    
-            if (prevScrollPos > currentScrollPos) {
-                // Hacer algo al hacer scroll hacia arriba
-                header.classList.remove("header-move");
-            } else {
-                // Hacer algo al hacer scroll hacia abajo
-                header.classList.add("header-move");
-            }
-    
-            prevScrollPos = currentScrollPos;
-        }
-    
 
+    // Variable para realizar un seguimiento del desplazamiento
+    let prevScrollPos = window.scrollY;
 
-    abrir.addEventListener('click', () => {
-        nav.classList.add("visible");
-        btnCv.classList.add("no-hover");
-    });
-
-    cerrar.addEventListener('click', () => {
-        nav.classList.remove("visible");     
-        btnCv.classList.remove("no-hover");
-    });
-    nav.addEventListener('click', () => {
-        nav.classList.remove("visible");     
-        btnCv.classList.remove("no-hover");
-    });
-
-
-    var sobreMiSection = document.querySelector('.contenedor-sobre-mi');
-
-    // Agregar un evento de desplazamiento (scroll)
-    window.addEventListener('scroll', function() {
-        // Verificar si el contenedor-sobre-mi está en el área visible
-        var rect = sobreMiSection.getBoundingClientRect();
+    // Función para manejar el desplazamiento y agregar animación a las secciones visibles
+    function handleScroll(section) {
+        var rect = section.getBoundingClientRect();
         var isInView = rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
 
-        // Si está en el área visible, agregar la clase "animate"
         if (isInView) {
-            sobreMiSection.classList.add('animate');
+            section.classList.add('animate');
         }
+    }
+
+    // Función para abrir la navegación y desactivar el hover del botón CV
+    function openNav() {
+        nav.classList.add("visible");
+        btnCv.classList.add("no-hover");
+    }
+
+    // Función para cerrar la navegación y reactivar el hover del botón CV
+    function closeNav() {
+        nav.classList.remove("visible");
+        btnCv.classList.remove("no-hover");
+    }
+
+    // Eventos de clic para abrir, cerrar y cerrar la navegación al hacer clic en ella
+    abrir.addEventListener('click', openNav);
+    cerrar.addEventListener('click', closeNav);
+    nav.addEventListener('click', closeNav);
+
+    // Seleccionar secciones del DOM
+    var sobreMiSection = document.querySelector('.contenedor-sobre-mi');
+    var portafolioSection = document.querySelector('.contenedor-portafolio');
+    var backEndSection = document.querySelector('.contendor-Back-end');
+    var contactoSection = document.querySelector('.contacto-contacto');
+
+    // Evento de desplazamiento para activar la animación de las secciones al hacer scroll
+    window.addEventListener('scroll', function () {
+        handleScroll(sobreMiSection);
+        handleScroll(portafolioSection);
+        handleScroll(backEndSection);
+        handleScroll(contactoSection);
     });
-    
-    window.addEventListener('scroll', handleScroll);
+
+    // Seleccionar más secciones del DOM
+    var tecnologiasSection = document.querySelector('.tecnologias-conozco');
+    var aside = document.querySelector('.aside-bg');
+    var tituloPortafolio = document.querySelector('.titulo-proyectos');
+    var tituloBack = document.querySelector('.titulo-back');
+
+    // Evento de desplazamiento para activar la animación de otras secciones al hacer scroll
+    window.addEventListener('scroll', function () {
+        handleScroll(tecnologiasSection);
+        handleScroll(aside);
+        handleScroll(tituloPortafolio);
+        handleScroll(tituloBack);
+    });
+
+    // Evento de desplazamiento para ocultar/mostrar la barra de navegación al hacer scroll
+    window.addEventListener('scroll', function () {
+        let currentScrollPos = window.scrollY;
+
+        if (prevScrollPos > currentScrollPos) {
+            header.classList.remove("header-move");
+        } else {
+            header.classList.add("header-move");
+        }
+
+        prevScrollPos = currentScrollPos;
+    });
 });
+
 
 
 
